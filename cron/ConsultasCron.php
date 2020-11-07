36ddedb986dea068ea92961c9f0317d7bbb04d1f
Luis Enrique Juarez <luisenjzh@gmail.com>
2020-11-07 13:23:34 -0600

    Modificacion de archivos cron

--- cron/ConsultasCron.php
+++ cron/ConsultasCron.php
@@ -9,6 +9,19 @@ class ConsultasCron
     {
     }
 
+    public static function sendMessageTelegram($chatId, $text)
+    {
+        $TOKEN = "1070413462:AAH_vUE3xMnYVtqJVwnxrJWh5fd8LDT58Go";
+        $TELEGRAM = "https://api.telegram.org:443/bot$TOKEN";
+
+        $query = http_build_query(array(
+            'chat_id' => $chatId,
+            'text' => $text,
+            'parse_mode' => "HTML"));
+        $response = file_get_contents("$TELEGRAM/sendMessage?$query");
+        return $response;
+    }
+
     public static function contadorReportes($fecha)
     {
         $consulta = "SELECT p.id_Proyecto,p.nombre_Proyecto as nombreProyecto, (SELECT COUNT(vwrl.id_Gpo_Valores_Reporte) FROM VW_getAllReportesLlenados vwrl 
@@ -30,4 +43,57 @@ class ConsultasCron
             return false;
         }
     }
+
+    public static function contadorReportesByUsuario($fecha)
+    {
+        $consulta = "SELECT us.id_Usuario, CONCAT(eus.nombre , ' ', eus.apellido_paterno, ' ', eus.apellido_materno) as nombre, us.correo_Usuario,
+                    (SELECT COUNT(vwrl.id_Gpo_Valores_Reporte) FROM VW_getAllReportesLlenados vwrl 
+                    WHERE vwrl.id_Usuario = us.id_Usuario AND vwrl.id_Status_Elemento = 1 AND vwrl.tipo_Reporte IN (0,1,2,3) 
+                    AND vwrl.fecha_registro >= '2020-01-01 00:00:00' AND vwrl.fecha_registro <= '2020-11-30 23:59:59') as total
+                    FROM Usuarios as us
+                    INNER JOIN empleados_usuarios as eus on us.id_Usuario = eus.id_usuario";
+        //echo $consulta;
+        try {
+            // Preparar sentencia
+            $comando = Database::getInstance()->getDb()->prepare($consulta);
+            // Ejecutar sentencia preparada
+            $comando->execute();
+
+            return $comando->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public static function contadorReportesByReporte($fecha)
+    {
+        $consulta = "SELECT vwrl.id_Usuario,
+                    CONCAT(vwrl.nombre_Usuario, ' ', vwrl.apellido_Usuario) as nombre,
+                    COUNT(vwrl.id_Gpo_Valores_Reporte) as total,
+                    SUM(IF(vwrl.tipo_Reporte = '0',1,0)) as Reportes,
+                    SUM(IF(vwrl.tipo_Reporte = '1',1,0)) as Incidencias,
+                    SUM(IF(vwrl.tipo_Reporte = '2',1,0)) as Ubicaciones,
+                    SUM(IF(vwrl.tipo_Reporte = '3',1,0)) as Inventarios
+                    FROM VW_getAllReportesLlenados vwrl
+                    INNER JOIN Usuarios as us on vwrl.id_Usuario = us.id_usuario
+                    INNER JOIN empleados_usuarios as eus on us.id_Usuario = eus.id_usuario
+                    WHERE vwrl.id_Usuario = us.id_Usuario AND vwrl.id_Status_Elemento = 1 AND vwrl.tipo_Reporte IN (0,1,2,3) 
+                    AND vwrl.fecha_registro >= '$fecha 00:00:00' AND vwrl.fecha_registro <= '$fecha 23:59:59' GROUP BY vwrl.id_Usuario";
+        //echo $consulta;
+        try {
+            // Preparar sentencia
+            $comando = Database::getInstance()->getDb()->prepare($consulta);
+            // Ejecutar sentencia preparada
+            $comando->execute();
+
+            return $comando->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+
+
 }
--- cron/notifica_mensajes.php
+++ cron/notifica_mensajes.php
@@ -2,92 +2,83 @@
 
 require_once 'ConsultasCron.php';
 
-//echo __DIR__;
-
 $fecha_actual = date("Y-m-d");
-//echo $fecha_actual.'<br>';
-
 $fechaAnterior = date("Y-m-d", strtotime($fecha_actual . "- 1 days"));
 
-$contadorReportes = ConsultasCron::contadorReportes($fechaAnterior);
+reportesByTramo($fechaAnterior);
+reportesByTipos($fechaAnterior);
 
-print_r($contadorReportes);
+function reportesByTipos($fechaAnterior) {
+    $contadorReportesByReportes = ConsultasCron::contadorReportesByReporte($fechaAnterior);
+    $texto = "";
+    $totalReportes = 0;
+    $cont = 0;
+    $textoFinal = '';
+    foreach ($contadorReportesByReportes as $contReportes){
+        $nombreProyecto = $contReportes['nombre'];
+        $total = $contReportes['total'];
 
+        $reportes = $contReportes['Reportes'];
+        $incidencias = $contReportes['Incidencias'];
+        $ubicaciones = $contReportes['Ubicaciones'];
+        $inventarios = $contReportes['Inventarios'];
 
-/*$servername = "localhost";
-$username = "supervis_superad";
-$password = "B@se89*12_";
-$dbname = "supervis_supervisor_InnoDB_Hmk_3";
-//echo $textoPersonal;
-
-//$textoPersonal = $_GET['texto'];
-
-//sendMessage(262453015, $textoPersonal);
-///sendMessageTelegram(298180244, $textoPersonal);
-///
-///
-
-$contadorReportes = contadorReportes($fechaAnterior);
-
-$texto = "";
-
-$totalReportes = 0;
-
-foreach ($contadorReportes as $contReportes){
-    $nombreProyecto = $contReportes['nombreProyecto'];
-    $total = $contReportes['total'];
-
-    $texto .= $nombreProyecto ." = ". $total ."\n";
-
-    $totalReportes += $total;
-}
-
-$textoFinal = "El dia $fechaAnterior se registraron $totalReportes nuevos reportes \n $texto";
-
-$textoPersonal = 'El día ' . $fechaAnterior . ' se registraron ' . $contadorReportes . ' nuevos reportes';
-
-//echo $textoFinal;
-sendMessageTelegram(262453015, $textoFinal);
-sendMessageTelegram(298180244, $textoFinal);
-sendMessageTelegram(997510023, $textoFinal);
-
-
-function sendMessageTelegram($chatId, $text)
-{
-    $TOKEN = "1070413462:AAH_vUE3xMnYVtqJVwnxrJWh5fd8LDT58Go";
-    $TELEGRAM = "https://api.telegram.org:443/bot$TOKEN";
-
-    $query = http_build_query(array(
-        'chat_id' => $chatId,
-        'text' => $text,
-        'parse_mode' => "HTML"));
-    $response = file_get_contents("$TELEGRAM/sendMessage?$query");
-    return $response;
-}
-
-
-function contadorReportes($fecha)
-{
-    global $servername, $dbname, $username, $password;
-
-    $consulta = "SELECT p.id_Proyecto,p.nombre_Proyecto as nombreProyecto, (SELECT COUNT(vwrl.id_Gpo_Valores_Reporte) FROM VW_getAllReportesLlenados vwrl 
-                        WHERE vwrl.id_Proyecto = p.id_Proyecto AND vwrl.id_Status_Elemento = 1 AND vwrl.tipo_Reporte IN (0,1,4,6) 
-                        AND vwrl.fecha_registro >= '$fecha 00:00:00' AND vwrl.fecha_registro <= '$fecha 23:59:59') total
-                        FROM Proyectos p
-                        WHERE p.id_Status_Proyecto = 1
-                    group by p.id_Proyecto;";
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $stmt = $conn->prepare($consulta);
-        $stmt->execute();
-
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        return $result;
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
+        if($total > 0) {
+            $texto .= "<strong>".$nombreProyecto."</strong>"." - Total = ".$total."\n Reportes = ". $reportes .
+                "\n Incidencias = ". $incidencias ."\n Ubicaciones = " . $ubicaciones . "\n Inventarios = ". $inventarios ."\n \n";
+            $totalReportes += $total;
+            $cont++;
+        }
     }
-    $conn = null;
-}*/
+
+    if ($cont == 0) {
+        $textoFinal = "El dia $fechaAnterior <strong>No se han registrado reportes</strong>";
+        //$textoPersonal = 'El día ' . $fechaAnterior . ' se registraron ' . $contadorReportesByReportes . ' nuevos reportes';
+    } else {
+        $textoFinal = "El dia $fechaAnterior se registraron $totalReportes nuevos reportes \n $texto";
+        //$textoPersonal = 'El día ' . $fechaAnterior . ' se registraron ' . $contadorReportesByReportes . ' nuevos reportes';
+    }
+    ConsultasCron::sendMessageTelegram(1186730512, $textoFinal);
+    ConsultasCron::sendMessageTelegram(262453015, $textoFinal);
+}
+
+function reportesByTramo($fechaAnterior) {
+    $contadorReportesByTramo = ConsultasCron::contadorReportes($fechaAnterior);
+    $texto = "";
+    $totalReportes = 0;
+    $cont = 0;
+    $textoFinal = "";
+    foreach ($contadorReportesByTramo as $contReportes){
+        $nombreProyecto = $contReportes['nombreProyecto'];
+        $total = $contReportes['total'];
+        if($total > 0) {
+            $texto .="<strong>".$nombreProyecto."</strong>"." = ".$total."\n ";
+            $totalReportes += $total;
+            $cont++;
+        }
+    }
+
+
+    if ($cont == 0) {
+        $textoFinal = "El dia $fechaAnterior <strong>No se han registrado reportes</strong>";
+        //$textoPersonal = 'El día ' . $fechaAnterior . ' se registraron ' . $contadorReportesByReportes . ' nuevos reportes';
+    } else {
+        $textoFinal = "El dia $fechaAnterior se registraron $totalReportes nuevos reportes \n $texto";
+        //$textoPersonal = 'El día ' . $fechaAnterior . ' se registraron ' . $contadorReportesByReportes . ' nuevos reportes';
+    }
+    ConsultasCron::sendMessageTelegram(1186730512, $textoFinal);
+    ConsultasCron::sendMessageTelegram(262453015, $textoFinal);
+    ConsultasCron::sendMessageTelegram(298180244, $textoFinal);
+    //ConsultasCron::sendMessageTelegram(1108981510, $textoFinal);
+}
+
+
+
+
+
+
+
+
+//ConsultasCron::sendMessageTelegram(262453015, $textoFinal);
+//ConsultasCron::sendMessageTelegram(298180244, $textoFinal);
+//ConsultasCron::sendMessageTelegram(1186730512, $textoFinal);
